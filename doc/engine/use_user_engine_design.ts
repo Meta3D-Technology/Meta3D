22c887bab4205ef23ff6466106ea639ea2cf2616
yyc-git <395976266@qq.com>
2022-02-18 07:55:58 +0800

    chore: update

---
+++ doc/engine/use_user_engine_design.ts
@@ -0,0 +1,24 @@
+import { createGameObject, init, update, render } from "./user_engine_design";
+
+function _createScene() {
+    let cube = createGameObject()
+    ...
+}
+
+_createScene()
+
+function _frame() {
+    update().then(() => {
+        render().then(() => {
+            requestAnimationFrame(() => {
+                _frame()
+            });
+        })
+    })
+}
+
+init().then(() => {
+    console.log("finish init")
+
+    _frame();
+})
