240f1d8a0b06b1b2daceb4deef08d0fefcfc9fce
yyc-git <395976266@qq.com>
2022-03-02 09:52:23 +0800

    refactor(components): transform: refactor Main.d.ts

--- components/meta3d-component-transform/src/Main.d.ts
+++ components/meta3d-component-transform/src/Main.d.ts
@@ -1,4 +1,4 @@
-import { componentContribute } from "meta3d-engine-core-protocol/src/contribute_points/scene_graph/ComponentContributeType";
+import { getComponentContribute as getComponentContributeMeta3D } from "meta3d-engine-core-protocol/src/contribute_points/scene_graph/ComponentContributeType";
 import { transform, dataName } from "meta3d-component-transform-protocol"
 
 export type config = {
@@ -10,4 +10,4 @@ export type config = {
 
 type state = any
 
-export function getComponentContribute(): componentContribute<state, config, dataName, transform>
+export let getComponentContribute: getComponentContributeMeta3D<state, config, dataName, transform>
