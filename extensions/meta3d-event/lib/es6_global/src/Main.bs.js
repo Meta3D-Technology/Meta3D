3e54eb7c1db0cf02ca02c81b98e5d98b520cedbc
yyc-git <395976266@qq.com>
2022-03-18 09:15:14 +0800

    chore(doc): update record.org

---
+++ extensions/meta3d-event/lib/es6_global/src/Main.bs.js
@@ -0,0 +1,23 @@
+
+
+import * as EventManager$Meta3dEvent from "./EventManager.bs.js";
+
+function getExtensionService(api, param) {
+  return {
+          trigger: (function (param, param$1, param$2, param$3) {
+              return EventManager$Meta3dEvent.trigger(api, param, param$1, param$2, param$3);
+            }),
+          registerEvent: EventManager$Meta3dEvent.registerEvent
+        };
+}
+
+function createExtensionState(param) {
+  return EventManager$Meta3dEvent.createExtensionState(undefined);
+}
+
+export {
+  getExtensionService ,
+  createExtensionState ,
+  
+}
+/* No side effect */
