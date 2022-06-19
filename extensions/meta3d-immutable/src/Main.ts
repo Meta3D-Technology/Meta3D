b9d1c283ff5012a6fd5b897fba342c08ce26a527
yyc-git <395976266@qq.com>
2022-06-20 06:45:20 +0800

    feat(extensions): add meta3d-immutable

---
+++ extensions/meta3d-immutable/src/Main.ts
@@ -0,0 +1,23 @@
+import { service } from "meta3d-immutable-protocol/src/service/ServiceType"
+import { dependentExtensionNameMap } from "meta3d-immutable-protocol/src/service/DependentExtensionType"
+import { dependentContributeNameMap } from "meta3d-immutable-protocol/src/service/DependentContributeType"
+import { state } from "meta3d-immutable-protocol/src/state/StateType"
+import { getExtensionService as getExtensionServiceMeta3D, createExtensionState as createExtensionStateMeta3D, getExtensionLife as getLifeMeta3D } from "meta3d-type"
+import { Map } from "immutable"
+
+export let getExtensionService: getExtensionServiceMeta3D<dependentExtensionNameMap, dependentContributeNameMap, service> = (_api, [_dependentExtensionNameMap, _dependentContributeNameMap]) => {
+    return {
+        createMap: () => Map(),
+        mapSet: (map, key, value) => map.set(key, value),
+        mapGet: (map, key) => map.get(key)
+    }
+}
+
+export let createExtensionState: createExtensionStateMeta3D<state> = () => {
+    return {}
+}
+
+export let getExtensionLife: getLifeMeta3D<service> = (api, extensionName) => {
+    return {
+    }
+}
