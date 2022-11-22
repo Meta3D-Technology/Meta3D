8b293c0fc70f796f58afc887d2efe45e3f015856
yyc-git <395976266@qq.com>
2022-11-23 05:53:21 +0800

    refactor: remove unused code

--- protocols/contribute_protocols/meta3d-skin-button-protocol/src/DependentMapType.ts
+++
@@ -1,3 +0,0 @@
-export type dependentExtensionNameMap = {}
-
-export type dependentContributeNameMap = {}
--- protocols/contribute_protocols/meta3d-skin-button-protocol/src/Index.ts
+++
@@ -1,7 +0,0 @@
-type color = [number, number, number]
-
-export type skin = {
-    normal: {
-        background_color: color
-    }
-}
