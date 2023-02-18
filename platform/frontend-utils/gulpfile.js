634cab6356a1ff24feb907228baaa694ea59d895
yyc-git <395976266@qq.com>
2023-02-18 08:15:47 +0800

    chore(package.json): add publish:patch script

--- platform/frontend-utils/gulpfile.js
+++ platform/frontend-utils/gulpfile.js
@@ -16,3 +16,18 @@ gulp.task("updateMinorVersion", function (done) {
 
     done()
 });
+
+gulp.task("updatePatchVersion", function (done) {
+    let versionFilePath = "./src/config/VersionConfig.res"
+
+    let content = fs.readFileSync(versionFilePath, {
+        encoding: 'utf8',
+    })
+
+    let currentVersion = content.match(/^let\sgetPlatformVersion\s\=\s\(\)\s\=>\s"(.+)"/im)[1]
+
+    fs.writeFileSync(versionFilePath, content.replace(/^let\sgetPlatformVersion\s\=\s\(\)\s\=>\s.+$/img, "let getPlatformVersion = () => " + "\"" + semver.inc(currentVersion, "patch") + "\"")
+    )
+
+    done()
+});
