121d5bb67d83a37599a3357e80d5d725ba350357
yyc-git <395976266@qq.com>
2023-12-20 14:04:46 +0800

    feat(input): add default inspector input

---
+++ contributes/meta3d-input-default-inspector/src/Main.ts
@@ -0,0 +1,15 @@
+import { state as meta3dState, getContribute as getContributeMeta3D } from "meta3d-type"
+import { data } from "meta3d-input-window-protocol"
+import { inputContribute } from "meta3d-editor-whole-protocol/src/service/ServiceType"
+import { actionName as selectSceneTreeNodeActionName, state as selectSceneTreeNodeState } from "meta3d-action-select-scenetree-node-protocol"
+
+export let getContribute: getContributeMeta3D<inputContribute<data>> = (api) => {
+    return {
+        inputName: "DefaultInspectorInput",
+        func: (meta3dState) => {
+            return Promise.resolve(api.nullable.isNullable(
+                api.nullable.bind(selectSceneTreeNodeState => selectSceneTreeNodeState.selectedGameObject, api.action.getActionState<selectSceneTreeNodeState>(meta3dState, selectSceneTreeNodeActionName))
+            ))
+        }
+    }
+}
