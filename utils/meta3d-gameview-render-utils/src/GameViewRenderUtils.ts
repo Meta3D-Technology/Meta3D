63442097c03cc83067d4cddae67e00dd5a394469
yyc-git <395976266@qq.com>
2024-01-28 11:25:20 +0800

    fix(action): fix "if change isActive / far when run,  arcball drag event not work" bug
    solution
    GameViewRenderUtils->runGameViewRenderOnlyOnce now not exec if isRun

--- utils/meta3d-gameview-render-utils/src/GameViewRenderUtils.ts
+++ utils/meta3d-gameview-render-utils/src/GameViewRenderUtils.ts
@@ -1,8 +1,13 @@
 import { api, state as meta3dState } from "meta3d-type"
 import { service as editorWholeService } from "meta3d-editor-whole-protocol/src/service/ServiceType"
 import { service as gameViewRenderService } from "meta3d-editor-gameview-render-protocol/src/service/ServiceType"
+import { actionName as runActionName, state as runState } from "meta3d-action-run-protocol"
 
 export let runGameViewRenderOnlyOnce = (meta3dState: meta3dState, api: api, { getPluggablePackageService }: editorWholeService): meta3dState => {
+    if (api.nullable.getWithDefault(api.nullable.map(runState => runState.isRun, api.action.getActionState<runState>(meta3dState, runActionName)), false)) {
+        return meta3dState
+    }
+
     return api.nullable.getWithDefault(
         api.nullable.map(
             ({ runOnlyOnce }) => {
