3ba15f82a9083b56708a1e8dc986d5f245289021
yyc-git <395976266@qq.com>
2026-03-19 11:05:24 +0800

    feat(editor): update quicktest action

--- contributes/meta3d-action-mod-unit-quicktest/src/Main.ts
+++ contributes/meta3d-action-mod-unit-quicktest/src/Main.ts
@@ -2,8 +2,11 @@ import { state as meta3dState, getContribute as getContributeMeta3D, api } from 
 // import { language } from "meta3d-action-mod-unit-add-careerfeature-protocol"
 import { actionContribute, service as editorWholeService } from "meta3d-editor-whole-protocol/src/service/ServiceType"
 import { actionName, state, uiData } from "meta3d-action-mod-unit-quicktest-protocol"
+import { actionName as initActionName, state as initState } from "meta3d-action-mod-unit-init-protocol"
 import { eventName, inputData } from "meta3d-action-mod-unit-quicktest-protocol/src/EventType"
-import { publish } from "meta3d-action-mod-unit-publish-utils/src/Main"
+import { publish, checkModData } from "meta3d-action-mod-unit-publish-utils/src/Main"
+import { getLanguageTextData } from "meta3d-language-utils/src/Main"
+import { languageKey } from "meta3d-language-utils/src/Type"
 
 export let getContribute: getContributeMeta3D<actionContribute<uiData, state>> = (api) => {
     return {
@@ -13,10 +16,21 @@ export let getContribute: getContributeMeta3D<actionContribute<uiData, state>> =
 
             return new Promise((resolve, reject) => {
                 resolve(eventSourcingService.on<inputData>(meta3dState, eventName, 0, (meta3dState) => {
+                    if (checkModData(api, [getLanguageTextData, languageKey], meta3dState, api.action.getActionState<initState>(meta3dState, initActionName))
+                    ) {
+                        return Promise.resolve(meta3dState)
+                    }
+
                     api.flow.deferExec(api, (meta3dState) => {
                         let author = "tempUser"
                         let name = `temp_unit`
-                        return publish(api, meta3dState, name, author).then(meta3dState => {
+                        return publish(api, meta3dState, name, author,
+                            "test1",
+                            "test1",
+                            "",
+                            false,
+                            ""
+                        ).then(meta3dState => {
                             // TODO restore
                             // window.open("https://www.gts-play.cn?quicktest", "_blank")
                             window.open("http://localhost:8093/?quicktest", "_blank")
