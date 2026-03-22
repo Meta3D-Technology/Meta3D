3f247a197d2924ac0554d12abf1b0f0370c282b5
yyc-git <395976266@qq.com>
2026-03-22 15:01:59 +0800

    feat(editor): update language set-xxx actions

--- contributes/meta3d-action-mod-language-set-en/src/Main.ts
+++ contributes/meta3d-action-mod-language-set-en/src/Main.ts
@@ -1,5 +1,5 @@
 import { state as meta3dState, getContribute as getContributeMeta3D } from "meta3d-type"
-import { language} from "meta3d-language-utils/src/Type"
+import { language } from "meta3d-language-utils/src/Type"
 import { actionName, state } from "meta3d-action-mod-language-set-en-protocol"
 import { eventName, inputData } from "meta3d-action-mod-language-set-en-protocol/src/EventType"
 import { actionContribute, service as editorWholeService } from "meta3d-editor-whole-protocol/src/service/ServiceType"
@@ -9,12 +9,14 @@ export let getContribute: getContributeMeta3D<actionContribute<null, state>> = (
     return {
         actionName: actionName,
         init: (meta3dState) => {
+            meta3dState = api.action.setActionState(meta3dState, languageActionName, Object.assign(Object.assign({}, api.action.getActionState(meta3dState, languageActionName)), { language: language.English }))
+
+
+
             let eventSourcingService = api.nullable.getExn(api.getPackageService<editorWholeService>(meta3dState, "meta3d-editor-whole-protocol")).event(meta3dState).eventSourcing(meta3dState)
 
             return new Promise((resolve, reject) => {
                 resolve(eventSourcingService.on<inputData>(meta3dState, eventName, 0, (meta3dState) => {
-                    meta3dState = api.action.setActionState(meta3dState, languageActionName, Object.assign(Object.assign({}, api.action.getActionState(meta3dState, languageActionName)), { language: language.English }))
-
                     return Promise.resolve(meta3dState)
                 }, (meta3dState) => {
                     return Promise.resolve(meta3dState)
