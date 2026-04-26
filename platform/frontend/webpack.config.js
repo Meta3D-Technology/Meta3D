3bed3415f67566d8740f4d9fcd95005205aab9e5
yyc-git <395976266@qq.com>
2026-04-26 15:19:51 +0800

    chore: update webpack.config.js

--- platform/frontend/webpack.config.js
+++ platform/frontend/webpack.config.js
@@ -135,27 +135,6 @@ module.exports = {
     ],
 
 
-    // 生产环境优化：代码分割、压缩
-    optimization: {
-        splitChunks: {
-            chunks: 'all',
-            cacheGroups: {
-                monaco: {
-                    test: /[\\/]node_modules[\\/]monaco-editor[\\/]/,
-                    name: 'monaco',
-                    chunks: 'all',
-                },
-                vendor: {
-                    test: /[\\/]node_modules[\\/]/,
-                    name: 'vendors',
-                    chunks: 'all',
-                    priority: -10,
-                },
-            },
-        },
-        minimize: true,
-    },
-
     // // When importing a module whose path matches one of the following, just
     // // assume a corresponding global variable exists and use that instead.
     // // This is important because it allows us to avoid bundling all of our
